f5de120cd359b340f786594edbf77f090e781972
euge7777 <2312972@ucc.edu.ar>
2024-08-19 09:06:58 -0300

    empezado

--- Ej1.cpp
+++ Ej1.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+#include "Lista/Lista.h"
 using namespace std;
 
 int main() {
 
-    cout << "Ejercicio N° 1" << endl;
-
+    Lista<int> Lista;
+    int n, d, p;
+    cout<<"Ingrede tamaño de la lista: ";
+    cin>>n;
+    for (int i = 0; i < n; i++){
+        cout << "Ingrese valor de la lista en la posición "<< i + 1 <<": ";
+        cin>>d;
+        Lista.insertarUltimo(d);
+    }
     return 0;
 }
